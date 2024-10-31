684ad98f40d7b2535250e7342f79da837364536d
Om Vasudev <omvasudev5055@gmail.com>
2024-10-31 15:06:48 +0530

    Initial Commit

---
+++ app/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#008000", // Customize your primary color
+    },
+    secondary: {
+      main: "#ff0000", // Customize your secondary color
+    },
+  },
+});
+
+export default theme;
